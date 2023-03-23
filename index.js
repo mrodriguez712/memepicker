bf8453d95a92baa89967086be66a320277b7f671
Marte Rodriguez <marte.rod0712@gmail.com>
2023-03-23 09:56:51 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,106 @@
+import { catsData } from './data.js'
+
+// variables
+const emotionsRadios = document.getElementById('emotion-radios')
+const getImage = document.getElementById('get-image-btn')
+const gifsOnlyOption = document.getElementById('gifs-only-option')
+const memeModalInner = document.getElementById('meme-modal-inner')
+const memeModal = document.getElementById('meme-modal')
+const memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
+
+// event listeners
+emotionsRadios.addEventListener('change', highlightCheckedOption)
+
+memeModalCloseBtn.addEventListener('click', () => memeModal.style.display = 'none')
+
+getImage.addEventListener('click', renderCat)
+
+// functions
+function highlightCheckedOption(e){
+    const radios = document.getElementsByClassName('radio')
+
+    for(let radio of radios) {
+        radio.classList.remove('highlight')
+    }
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
+function renderCat() {
+    const catsObject = getSingleCatObject()
+    memeModal.style.display = 'flex'
+    memeModalInner.innerHTML = 
+    `
+        <img 
+        class="cat-img" 
+        src="./images/${catsObject.image}"
+        alt="${catsObject.alt}"
+        >
+    `
+}
+
+function getSingleCatObject() {
+    const catsArray = getMatchingCatsArray()
+    
+    if(catsArray.length === 1) {
+        return catsArray[0]
+    } else {
+        const catRandom = Math.floor(Math.random() * catsArray.length)
+        return catsArray[catRandom]
+    }
+}
+
+function getMatchingCatsArray(){     
+    if(document.querySelector('input[type="radio"]:checked')){
+        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+        const isGif = gifsOnlyOption.checked
+        
+        const matchingCatsArray = catsData.filter(function(cat){
+            
+            if(isGif){
+                return cat.emotionTags.includes(selectedEmotion) && cat.isGif
+            }
+            else{
+                return cat.emotionTags.includes(selectedEmotion)
+            }            
+        })
+        return matchingCatsArray 
+    }   
+}
+
+function renderEmotionsRadios(cats) {
+    let radioItems = ''
+    const emotions = getEmotionsArray(cats)
+    
+    for(let emotion of emotions) {
+        radioItems += 
+        `
+        <div class='radio'>
+            <label for="${emotion}">${emotion}</label>
+            <input 
+            type="radio"
+            id='${emotion}'
+            value='${emotion}'
+            name="emotion"
+            />
+        </div>
+        
+        `
+    }
+
+    emotionsRadios.innerHTML = radioItems
+}
+
+renderEmotionsRadios(catsData)
+
+function getEmotionsArray(cats) {
+    const emotionsArray = []
+
+    for(let cat of cats) {
+        for(let emotion of cat.emotionTags) {
+            if(!emotionsArray.includes(emotion)) {
+                emotionsArray.push(emotion)
+            }
+        }
+    }
+    return emotionsArray
+}
